ff84545cffc88ed5e4d01b671f9e98a9a2a00117
Altamash Shaikh <mr.shaikh.altamash0203@gmail.com>
2025-08-25 19:46:28 +0530

    Home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,67 +2,257 @@ import React from "react";
 
 function HomePage() {
   return (
-    <div className="flex justify-between">
-      {/* Hero Section */}
-      <section className="text-center py-5 ">
-        <h1>
-          From Ideas To Impact
+    <div>
+      {/* First */}
+      <div className="h-auto bg-yellow-200  shadow-xs rounded-xl shadow-neutral-700 ">
+        <div className="flex justify-evenly items-center flex-wrap">
+          <img
+            className="bg-white  mt-2 rounded-full h-60 w-60 p-2"
+            src="/Slider/ChatGPT Image Aug 24, 2025, 12_37_10 PM.png"
+            alt=""
+          />
+          <img
+            className="bg-white  mt-2 rounded-full h-60 w-60 p-2"
+            src="/Slider/ChatGPT Image Aug 24, 2025, 12_38_47 PM.png"
+            alt=""
+          />
+          <img
+            className="bg-white  mt-2 rounded-full h-60 w-60 p-2"
+            src="/Slider/ChatGPT Image Aug 24, 2025, 12_38_50 PM.png"
+            alt=""
+          />
+          <img
+            className="bg-white  mt-2 rounded-full h-60 w-60 p-2"
+            src="/Slider/ChatGPT Image Aug 24, 2025, 12_38_53 PM.png"
+            alt=""
+          />
+        </div>
+
+        <hr />
+
+        <p className="text-center text-l italic text-black font-light ">
+          <span className="font-extrabold text-xl">"</span>Fundbook empowers
+          dreamers, innovators, and changemakers <br /> to bring their ideas to
+          life. With zero platform fees and <br />a supportive community,
+          raising funds has never been easier.
+          <span className="font-extrabold text-xl">"</span>
+        </p>
+        <hr />
+      </div>
+      {/* First End */}
+      {/* Second  */}
+      <div className="mt-10 px-4 ">
+        <h3 className="text-center tracking-widest ">
+          You Can Raise Funds For?
           <br />
-          Raise Fund Easily
-          <br />
-          On Fundbook
-        </h1>
-      </section>
-
-      {/* Featured Campaigns Section */}
-      <section className="container py-5">
-        <h2 className="mb-4 text-center">Our Featured Campaigns</h2>
-
-        {/* Carousel Component */}
-        <div
-          id="fundbookCarousel"
-          className="carousel slide"
-          data-bs-ride="carousel"
-        >
-
-          {/* Carousel Inner */}
-          <div className="carousel-inner ">
-            <div className="carousel-item active">
+          <span className="tracking-tighter font-extrabold text-cyan-500">
+            ___________________
+          </span>
+        </h3>
+        <div className="flex justify-center gap-6 items-center flex-wrap  mt-10  ">
+          <div className="border p-10 rounded shadow-[0px_20px_30px_-10px_rgb(38,_57,_77)]">
+            <div>
               <img
-                src="https://picsum.photos/800/400?random=1"
-                className="d-block  w-100 rounded-full"
-                alt="Campaign 1"
+                src="https://cdn-icons-png.flaticon.com/128/1067/1067256.png"
+                width={40}
+                alt=""
               />
-              <div className="carousel-caption d-none d-md-block">
-              </div>
-            </div>
-            <div className="carousel-item">
-              <img
-                src="https://picsum.photos/800/400?random=2"
-                className="d-block  rounded-full"
-                alt="Campaign 2"
-              />
-              <div className="carousel-caption d-none d-md-block">
-                <h5>Campaign Two</h5>
-                <p>Empowering communities through funding.</p>
-              </div>
-            </div>
-            <div className="carousel-item">
-              <img
-                src="https://picsum.photos/800/400?random=3"
-                className="d-block rounded-full"
-                alt="Campaign 3"
-              />
-              <div className="carousel-caption d-none d-md-block">
-                <h5>Campaign Three</h5>
-                <p>Turning dreams into reality with support.</p>
-              </div>
             </div>
           </div>
-
-        
+          <div className="border p-10 rounded shadow-[0px_20px_30px_-10px_rgb(38,_57,_77)]">
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/2966/2966334.png"
+              width={40}
+              alt=""
+            />
+          </div>
+          <div className="border p-10 rounded shadow-[0px_20px_30px_-10px_rgb(38,_57,_77)]">
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/10428/10428527.png"
+              width={40}
+              alt=""
+            />
+          </div>
+          <div className="border p-10 rounded shadow-[0px_20px_30px_-10px_rgb(38,_57,_77)]">
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/2648/2648958.png"
+              width={40}
+              alt=""
+            />
+          </div>
+          <div className="border p-10 rounded shadow-[0px_20px_30px_-10px_rgb(38,_57,_77)]">
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/8909/8909186.png"
+              width={40}
+              alt=""
+            />
+          </div>
         </div>
-      </section>
+      </div>
+      {/* second end */}
+
+
+      {/* active Funraisers */}
+
+  
+
+      {/* Third */}
+
+      <div className="mt-16  px-4 h-auto ">
+        <h3 className="text-center tracking-widest ">
+          Why People Choose Fundbook?
+          <br />
+          <span className="tracking-tighter font-extrabold text-cyan-500">
+            ___________________
+          </span>
+        </h3>
+
+        <div className="flex  flex-wrap justify-around items-center bg-gray-200 p-10 gap-20">
+          <div className="flex flex-col justify-center items-center border-1 p-2 shadow-xl rounded ">
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/3706/3706340.png"
+              width={40}
+              alt=""
+            />
+            <b>Zero Platform Fee</b>
+            <p className="text-center font-light italic leading-8">
+              Start your crowdfunding journey with
+              <br /> zero platform fee. More support, more impact.
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center border-1 p-2 shadow-xl rounded ">
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/1180/1180217.png"
+              width={40}
+              alt=""
+            />
+            <b>No Hidden Charges</b>
+            <p className="text-center font-light italic leading-8">
+              What you see is what you get <br /> transparent crowdfunding with
+              no hidden charges.
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center border-1 p-2 shadow-xl rounded ">
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/2612/2612924.png"
+              width={40}
+              alt=""
+            />
+            <b>Raise Campaign in 2 min</b>
+            <p className="text-center font-light italic leading-8">
+              Start your campaign instantly — set up fundraising <br /> in just
+              2 minutes and share with the world.
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center border-1 p-2 shadow-2xl rounded ">
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/11872/11872350.png"
+              width={40}
+              alt=""
+            />
+            <b>Easy User Interface</b>
+            <p className="text-center font-light italic leading-8">
+              No complexity, no confusion. A clean, user-friendly <br />{" "}
+              interface built to guide you every step.
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* third end */}
+
+      {/* Fourth*/}
+
+      <div className="mt-10 p-10">
+        <h3 className="text-center tracking-widest ">
+          Hear From Our Users
+          <br />
+          <span className="tracking-tighter font-extrabold text-cyan-500">
+            ___________________
+          </span>
+        </h3>
+
+        <div className="shadow-2xl shadow-black">
+          <marquee behavior="" direction="" scrollamount="15">
+            <div className="flex justify-center items-center gap-5 p-10">
+              <div className="flex flex-col justify-center items-center text-center border-1 rounded-2xl p-3 ">
+                <p className="font-extralight">
+                  We would urge anyone and everyone to extend their helping hand <br />
+                  in betterment of a person who will continuously make this <br />
+                  world a better place. Any contribution you make, big or small,<br />
+                  will put us in your debt forever.”
+                </p>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgDLNDUELozWfe9vauq2l42glvbiUOSAIOWg&s"
+                  className="w-10 h-10 rounded-full"
+                  alt=""
+                />
+                <h6 className="font-extralight">User!</h6>
+              </div>
+
+               <div className="flex flex-col justify-center items-center text-center border-1 rounded-2xl p-3 ">
+                <p className="font-extralight">
+                  We would urge anyone and everyone to extend their helping hand <br />
+                  in betterment of a person who will continuously make this <br />
+                  world a better place. Any contribution you make, big or small,<br />
+                  will put us in your debt forever.”
+                </p>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgDLNDUELozWfe9vauq2l42glvbiUOSAIOWg&s"
+                  className="w-10 h-10 rounded-full"
+                  alt=""
+                />
+                <h6 className="font-extralight">User!</h6>
+              </div>
+
+               <div className="flex flex-col justify-center items-center text-center border-1 rounded-2xl p-3 ">
+                <p className="font-extralight">
+                  We would urge anyone and everyone to extend their helping hand <br />
+                  in betterment of a person who will continuously make this <br />
+                  world a better place. Any contribution you make, big or small,<br />
+                  will put us in your debt forever.”
+                </p>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgDLNDUELozWfe9vauq2l42glvbiUOSAIOWg&s"
+                  className="w-10 h-10 rounded-full"
+                  alt=""
+                />
+                <h6 className="font-extralight">User!</h6>
+              </div>
+
+               <div className="flex flex-col justify-center items-center text-center border-1 rounded-2xl p-3 ">
+                <p className="font-extralight">
+                  We would urge anyone and everyone to extend their helping hand <br />
+                  in betterment of a person who will continuously make this <br />
+                  world a better place. Any contribution you make, big or small,<br />
+                  will put us in your debt forever.”
+                </p>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgDLNDUELozWfe9vauq2l42glvbiUOSAIOWg&s"
+                  className="w-10 h-10 rounded-full"
+                  alt=""
+                />
+                <h6 className="font-extralight">User!</h6>
+              </div>
+
+               <div className="flex flex-col justify-center items-center text-center border-1 rounded-2xl p-3 ">
+                <p className="font-extralight">
+                  We would urge anyone and everyone to extend their helping hand <br />
+                  in betterment of a person who will continuously make this <br />
+                  world a better place. Any contribution you make, big or small,<br />
+                  will put us in your debt forever.”
+                </p>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgDLNDUELozWfe9vauq2l42glvbiUOSAIOWg&s"
+                  className="w-10 h-10 rounded-full"
+                  alt=""
+                />
+                <h6 className="font-extralight">User!</h6>
+              </div>
+            </div>
+          </marquee>
+        </div>
+      </div>
+      {/* Fourth End */}
     </div>
   );
 }
